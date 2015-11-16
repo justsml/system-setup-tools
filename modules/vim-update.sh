bac0b65d7d33817404c6f60eb4c590d528bf17ce
Dan Levy <justsml@gmail.com>
2015-11-16 03:05:00 -0700

    vim auto installer script enhancements

--- modules/vim-update.sh
+++ modules/vim-update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 set -e
 
 
@@ -7,7 +8,7 @@ function check_paths () {
 		mkdir -p ~/.vim/colors
 		printf "VIM: created folder             ~/.vim/colors\n"
 	else
-		printf "VIM: folder already exists      ~/.vim/colors\n"
+		printf "VIM: Success: folder already exists      ~/.vim/colors\n"
 	fi
 
 	if [ ! -f ~/.vimrc ]; then
@@ -18,40 +19,66 @@ function check_paths () {
 		EOF
 		printf "VIM: created file               ~/.vimrc\n"
 	else
-		printf "VIM: file already exists        ~/.vimrc\n"
+		printf "VIM: Success: file already exists        ~/.vimrc\n"
 	fi
 }
 
 function get_themes () {
+	# printf "VIM: checking for themes \n"
 	cd ~/.vim/colors
-	[ ! -e ./onedark.vim ] && wget https://raw.githubusercontent.com/geoffharcourt/one-dark.vim/master/colors/onedark.vim
-	[ ! -e ./ubaryd.vim ] && wget https://raw.githubusercontent.com/Donearm/Ubaryd/master/colors/ubaryd.vim
-	[ ! -e ./antares.vim ] && wget https://raw.githubusercontent.com/Haron-Prime/Antares/master/colors/antares.vim
-	[ ! -e ./molokai_dark.vim ] && wget https://raw.githubusercontent.com/fcevado/molokai_dark/master/colors/molokai_dark.vim
-	[ ! -e ./materialbox.vim ] && wget https://raw.githubusercontent.com/mkarmona/materialbox/master/colors/materialbox.vim
-	[ ! -e ./colorsbox-stnight.vim ] && wget https://raw.githubusercontent.com/mkarmona/colorsbox/master/colors/colorsbox-stnight.vim
-	[ ! -e ./colorsbox-faff.vim ] && wget https://raw.githubusercontent.com/mkarmona/colorsbox/master/colors/colorsbox-faff.vim
-	[ ! -e ./colorsbox-stblue.vim ] && wget https://raw.githubusercontent.com/mkarmona/colorsbox/master/colors/colorsbox-stblue.vim
-	[ ! -e ./colorsbox-stbright.vim ] && wget https://raw.githubusercontent.com/mkarmona/colorsbox/master/colors/colorsbox-stbright.vim
-	[ ! -e ./colorsbox-material.vim ] && wget https://raw.githubusercontent.com/mkarmona/colorsbox/master/colors/colorsbox-material.vim
+	[ ! -e ./onedark.vim ] && \
+		curl --progress-bar \
+			-o ./onedark.vim -SL https://raw.githubusercontent.com/geoffharcourt/one-dark.vim/master/colors/onedark.vim
+	[ ! -e ./ubaryd.vim ] && \
+		curl --progress-bar \
+			-o ./ubaryd.vim -SL https://raw.githubusercontent.com/Donearm/Ubaryd/master/colors/ubaryd.vim
+	[ ! -e ./antares.vim ] && \
+		curl --progress-bar \
+			-o ./antares.vim -SL https://raw.githubusercontent.com/Haron-Prime/Antares/master/colors/antares.vim
+	[ ! -e ./molokai_dark.vim ] && \
+		curl --progress-bar \
+			-o ./molokai_dark.vim -SL https://raw.githubusercontent.com/fcevado/molokai_dark/master/colors/molokai_dark.vim
+	[ ! -e ./materialbox.vim ] && \
+		curl --progress-bar \
+			-o ./materialbox.vim -SL https://raw.githubusercontent.com/mkarmona/materialbox/master/colors/materialbox.vim
+	[ ! -e ./colorsbox-stnight.vim ] && \
+		curl --progress-bar \
+			-o ./colorsbox-stnight.vim -SL https://raw.githubusercontent.com/mkarmona/colorsbox/master/colors/colorsbox-stnight.vim
+	[ ! -e ./colorsbox-faff.vim ] && \
+		curl --progress-bar \
+			-o ./colorsbox-faff.vim -SL https://raw.githubusercontent.com/mkarmona/colorsbox/master/colors/colorsbox-faff.vim
+	[ ! -e ./colorsbox-stblue.vim ] && \
+		curl --progress-bar \
+			-o ./colorsbox-stblue.vim -SL https://raw.githubusercontent.com/mkarmona/colorsbox/master/colors/colorsbox-stblue.vim
+	[ ! -e ./colorsbox-stbright.vim ] && \
+		curl --progress-bar \
+			-o ./colorsbox-stbright.vim -SL https://raw.githubusercontent.com/mkarmona/colorsbox/master/colors/colorsbox-stbright.vim
+	[ ! -e ./colorsbox-material.vim ] && \
+		curl --progress-bar \
+			-o ./colorsbox-material.vim -SL https://raw.githubusercontent.com/mkarmona/colorsbox/master/colors/colorsbox-material.vim
+	printf "VIM: Success: themes \n"
+	cd ~/
 }
 
 function vim_check () {
 	is_installed=$(dpkg --status vim-nox | grep 'Status:.*install')
 	if [ ! -e /etc/debian_version ]; then
-		printf "\nUNSUPPORTED: Currently only debian-base supported\n\n"
+		printf "\nUNSUPPORTED: Currently only debian-base supported\n\n\n"
 		sleep 3s
 		exit -99
 	fi
 
 	if [[ $is_installed =~ "install" ]]; then
-		printf "VIM: Success: vim-nox package already installed\n\n\n"
+		printf "VIM: Success: vim-nox package already installed\n"
 	else
-		printf "VIM: installing needed packages: vim-nox\n\n\n"
-		sudo apt-get update && apt-get install -y vim-nox wget
+		printf "VIM: installing needed packages: vim-nox\n"
+		sudo apt-get update && \
+				 apt-get install -y vim-nox curl
 	fi
 }
 
 check_paths
 vim_check
 get_themes
+
+
