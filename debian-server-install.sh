2b5834cc764c2dfdf26b060afa53e3aedbf57eb6
Daniel Levy <justsml@users.noreply.github.com>
2016-01-29 15:02:11 -0700

    Update debian-server-install.sh

--- debian-server-install.sh
+++ debian-server-install.sh
@@ -7,7 +7,7 @@ echo 'deb http://httpredir.debian.org/debian jessie-updates main contrib non-fre
 echo 'deb http://security.debian.org jessie/updates main contrib non-free' >> /etc/apt/sources.list
 sudo apt-get update && \
   sudo apt-get install -y \
-    ufw dbus rlwrap qshutdown vim-nox build-essential \
+    ufw fail2ban dbus rlwrap qshutdown vim-nox build-essential \
     libssl-dev zlib1g-dev curl wget checkinstall python-dev \
     devscripts dh-make p7zip-full zip unzip apt-utils g++ flex bison gperf \
     ruby perl python-setuptools libssl-dev git git-core \
