fe1e3514f24c3cddc215b3473c9dbe652c05ab1c
Daniel Levy <justsml@users.noreply.github.com>
2016-01-29 14:51:36 -0700

    added ufw + sleep b4 dist-upgrade

--- debian-server-install.sh
+++ debian-server-install.sh
@@ -7,7 +7,7 @@ echo 'deb http://httpredir.debian.org/debian jessie-updates main contrib non-fre
 echo 'deb http://security.debian.org jessie/updates main contrib non-free' >> /etc/apt/sources.list
 sudo apt-get update && \
   sudo apt-get install -y \
-    dbus rlwrap qshutdown vim-nox build-essential \
+    ufw dbus rlwrap qshutdown vim-nox build-essential \
     libssl-dev zlib1g-dev curl wget checkinstall python-dev \
     devscripts dh-make p7zip-full zip unzip apt-utils g++ flex bison gperf \
     ruby perl python-setuptools libssl-dev git git-core \
@@ -15,8 +15,12 @@ sudo apt-get update && \
     libpng-dev libjpeg-dev libx11-dev libxext-dev \
     ttf-freefont ttf-mscorefonts-installer ttf-bitstream-vera ttf-dejavu ttf-liberation
 
+sleep 2s
+
 # Install VI Prefs - Recommended - Optional
 curl -o- https://raw.githubusercontent.com/justsml/system-setup-tools/master/modules/vim-update.sh | sudo bash
+
+sleep 2s
 
 # Backup Profile init scripts before appending new scripts below
 mkdir ~/backups
@@ -26,3 +30,8 @@ curl -sSL https://raw.githubusercontent.com/justsml/system-setup-tools/master/ho
 curl -sSL https://raw.githubusercontent.com/justsml/system-setup-tools/master/home-scripts/.bash_aliases > ~/.bash_aliases
 # Read into current shell (login steps already missed the aliases file)
 source ~/.bashrc
+
+sleep 5s
+
+echo Completing System Upgrade
+sudo apt-get dist-upgrade -y 
