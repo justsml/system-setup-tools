817e3d16025be94dbf7a0db51057ac547bd66f73
Daniel Levy <justsml@users.noreply.github.com>
2016-11-05 04:41:26 -0600

    Create letsencrypt-docker.sh

---
+++ letsencrypt-docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+domain_list=$1
+domain_list=${domain_list-"example.com"}
+
+docker run -it --rm -p 443:443 -p 80:80 --name certbot \
+            -v "/etc/letsencrypt:/etc/letsencrypt" \
+            -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
+            quay.io/letsencrypt/letsencrypt:latest certonly \
+            --noninteractive \
+            --expand --allow-subset-of-names \
+            --agree-tos --rsa-key-size 4096 \
+            --email postmaster@elasticsuite.com \
+            --domains $domain_list
+            
+
