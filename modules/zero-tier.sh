d0d3c2a1c0c3e6602c471e129b52196e9ab106fd
Daniel Levy <justsml@users.noreply.github.com>
2016-11-22 17:50:23 -0700

    Update zero-tier.sh

--- modules/zero-tier.sh
+++ modules/zero-tier.sh
@@ -3,7 +3,7 @@
 curl -s https://raw.githubusercontent.com/zerotier/ZeroTierOne/master/doc/contact%40zerotier.com.gpg | gpg --import
 ZT_RESULTS=$(curl -s https://install.zerotier.com/ | gpg --output - >/tmp/zt-install.sh && bash /tmp/zt-install.sh)
 
-echo $ZT_RESULTS > $HOME/zerotier-install.log
+printf $ZT_RESULTS > $HOME/zerotier-install.log
 
 cat $HOME/zerotier-install.log
 
