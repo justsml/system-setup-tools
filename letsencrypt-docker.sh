7e97aa59e9c21ec71d54e4e115605796cecc709d
Daniel Levy <justsml@users.noreply.github.com>
2017-01-02 04:27:58 -0700

    Fix & added usage instructions

--- letsencrypt-docker.sh
+++ letsencrypt-docker.sh
@@ -1,16 +1,37 @@
-#!/bin/bash -e
+#!/bin/bash
+set -e
 
-domain_list=$1
-domain_list=${domain_list-"example.com"}
+# USAGE
+# export HOSTNAMES=danlevy.net,www.danlevy.net,app.danlevy.net && curl -L https://raw.githubusercontent.com/justsml/system-setup-tools/master/letsencrypt-docker.sh | DOMAIN_CSV_LIST="$HOSTNAMES" bash
 
-docker run -it --rm -p 443:443 -p 80:80 --name certbot \
+domain_list=${DOMAIN_CSV_LIST-"`hostname -f`"}
+first_domain=$(echo $domain_list | sed 's/,/\n/g' | head -n 1)
+
+if [ -d "/etc/letsencrypt/live/$first_domain" ]; then
+  docker run -it --rm -p 443:443 -p 80:80 --name certbot \
+            -v "/etc/letsencrypt:/etc/letsencrypt" \
+            -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
+            quay.io/letsencrypt/letsencrypt:latest renew
+else
+  docker run -it --rm -p 443:443 -p 80:80 --name certbot \
             -v "/etc/letsencrypt:/etc/letsencrypt" \
             -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
             quay.io/letsencrypt/letsencrypt:latest certonly \
-            --noninteractive \
+            standalone --noninteractive \
             --expand --allow-subset-of-names \
-            --agree-tos --rsa-key-size 4096 \
-            --email postmaster@example.com \
+            --agree-tos --rsa-key-size 2048 \
+            --email postmaster@$first_domain \
             --domains $domain_list
-            
+fi
 
+# Update certs in shared 'local' or volume-mounted cert location
+mkdir -p /certs/$first_domain
+cp -raL /etc/letsencrypt/live/$first_domain/*.pem /certs/$first_domain/
+
+# Now, always override local/vol-mounted /certs/*.pem
+cp -raL /certs/$first_domain/* /certs/
+
+# # override if no default /certs/ keys exist already
+# if [ ! -f /certs/fullchain.pem ]; then
+#  cp -raL /certs/$first_domain/* /certs/
+# fi
