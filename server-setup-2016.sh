690fd09353d6d3511e5177e332e56c96026c537b
Daniel Levy <justsml@users.noreply.github.com>
2017-01-14 14:33:40 -0700

    More tuning: process, socket, and file limits

--- server-setup-2016.sh
+++ server-setup-2016.sh
@@ -97,34 +97,61 @@ function setupDHParam () {
 function setupLimits() {
   if [ ! -f "/etc/security/limits.conf" ]; then
     cat << HEREDOC >> /etc/security/limits.conf
-soft nofile 8192
-hard nofile 8192
+soft nofile 65535
+hard nofile 65535
 HEREDOC
   fi
   if [ "$(egrep "soft nofile" /etc/security/limits.conf)" == "" ]; then
-    echo "soft nofile 8192" >> /etc/security/limits.conf
+    echo "soft nofile 65535" >> /etc/security/limits.conf
   fi
   if [ "$(egrep "hard nofile" /etc/security/limits.conf)" == "" ]; then
-    echo "hard nofile 8192" >> /etc/security/limits.conf
+    echo "hard nofile 65535" >> /etc/security/limits.conf
   fi  
 }
 
 function serverSystemTuning () {
-  if [ "$(egrep "mem_max.?=.?[0-9]{7,}" /etc/sysctl.conf)" == "" ]; then
+  # Set high thread & process limits - ideal for servers w/ +16GB RAM
+  if [ "$(egrep "kernel.threads-max" /etc/sysctl.conf)" == "" ]; then
     cat << HEREDOC >> /etc/sysctl.conf
-# 16MB per socket - which sounds like a lot, but will virtually never
-# consume that much.
-net.core.rmem_max = 16777216
-net.core.wmem_max = 16777216
+kernel.threads-max = 256000
+HEREDOC
+  fi
+  if [ "$(egrep "kernel.max_pid" /etc/sysctl.conf)" == "" ]; then
+    cat << HEREDOC >> /etc/sysctl.conf
+kernel.pid_max = 4194303
+HEREDOC
+  fi
+  if [ "$(egrep "vm.max_map_count" /etc/sysctl.conf)" == "" ]; then
+    cat << HEREDOC >> /etc/sysctl.conf
+vm.max_map_count = 600000
 HEREDOC
   fi
 
+  # Tune NETWORK configuration for 2-10G+ Ethernet + Many Connections
+  # Credit: https://fasterdata.es.net/host-tuning/linux/
+  if [ "$(egrep "mem_max.?=.?[0-9]{7,}" /etc/sysctl.conf)" == "" ]; then
+    cat << HEREDOC >> /etc/sysctl.conf
+# Allow up to 64MB per socket - it will virtually never consume that much.
+net.core.rmem_max = 67108864 
+net.core.wmem_max = 67108864 
+HEREDOC
+  fi
+  if [ "$(egrep "tcp_rmem" /etc/sysctl.conf)" == "" ]; then
+    cat << HEREDOC >> /etc/sysctl.conf
+# increase Linux autotuning TCP buffer limit to 32MB
+net.ipv4.tcp_rmem = 4096 87380 33554432
+net.ipv4.tcp_wmem = 4096 65536 33554432
+HEREDOC
+  fi
   if [ "$(egrep "tcp_syncookies" /etc/sysctl.conf)" == "" ]; then
     cat << HEREDOC >> /etc/sysctl.conf
 net.ipv4.tcp_syncookies = 1
+net.ipv4.tcp_syn_retries = 2
+net.ipv4.tcp_synack_retries = 2
+net.ipv4.tcp_max_syn_backlog = 8192
+net.ipv4.tcp_fin_timeout = 8
 HEREDOC
   fi
-
 # Credit: https://www.linode.com/docs/websites/nginx/configure-nginx-for-optimized-performance
   if [ "$(egrep "tcp_max_tw_buckets" /etc/sysctl.conf)" == "" ]; then
     cat << HEREDOC >> /etc/sysctl.conf
@@ -188,6 +215,10 @@ enableSSHKeepAlive
 setupDHParam
 serverSystemTuning
 setupLimits
+
+printf "\n===============\n============\nServer sysctl.conf Config:\n\n"
+cat /etc/sysctl.conf
+printf "\n===============\n============\n\n\n\n"
 
 if [ ! -f "$HOME/.ssh/id_rsa" ]; then
   printf '\n\n### Attempting SSH Key Check & AutoGenerator \n'
