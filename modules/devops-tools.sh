998353e36810b7b8f9412bb054817f4b5d162efa
Daniel Levy <justsml@users.noreply.github.com>
2017-01-19 17:46:08 -0700

    Create devops-tools.sh

---
+++ modules/devops-tools.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+export  RANCHER_COMPOSE_VERSION=0.12.1 \
+        DOCKER_COMPOSE_VERSION=1.9.0 \
+        DOCKER_VERSION=1.12.5
+
+### Install binaries ###
+sudo curl -fsSLO https://get.docker.com/builds/Linux/x86_64/docker-$DOCKER_VERSION.tgz
+sudo tar --strip-components=1 -xzf docker-$DOCKER_VERSION.tgz -C /usr/local/bin 
+sudo chmod +x /usr/local/bin/docker
+
+sudo curl -sL "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" > /usr/local/bin/docker-compose 
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo curl -sL "https://github.com/rancher/rancher-compose/releases/download/$RANCHER_COMPOSE_VERSION/rancher-compose-linux-amd64-$RANCHER_COMPOSE_VERSION.tar.gz" > /tmp/rancher-compose.tar.gz 
+sudo tar --strip-components=1 -xzf /tmp/rancher-compose.tar.gz -C /usr/local/bin 
+sudo chmod +x /usr/local/bin/rancher-compose
+
+printf "\n\nVersions:\n"
+printf "  Docker: $(docker --version)\n"
+printf "  Docker Compose: $(docker-compose --version)\n"
+printf "  Rancher Compose: $(rancher-compose --version)\n"
+printf "===============================\n\n\n"
