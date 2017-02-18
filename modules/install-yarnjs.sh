0d6203a3bc14258a5a8a30f2befa07a7dec6748f
Daniel Levy <justsml@users.noreply.github.com>
2017-02-18 16:08:27 -0700

    Create install-yarnjs.sh

---
+++ modules/install-yarnjs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+sudo apt-get update >/dev/null && \
+     sudo apt-get install yarn -y -qq
+
