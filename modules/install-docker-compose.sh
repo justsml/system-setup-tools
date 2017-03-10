76582a019b8fda6b9bdfd756f0f0c94ec9f1a719
Daniel Levy <justsml@users.noreply.github.com>
2017-03-09 21:17:34 -0700

    Update install-docker-compose.sh

--- modules/install-docker-compose.sh
+++ modules/install-docker-compose.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$(which docker-compose)" == "" ]; then
-  curl -L "https://github.com/docker/compose/releases/download/1.11.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  curl -L "https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   chmod +x /usr/local/bin/docker-compose
   curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
   printf "\n### docker-compose updated! $(docker-compose --version)\n"
