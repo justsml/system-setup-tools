5d2f176b3cd119ee28da1f85bf0734cad2fdaffa
Daniel Levy <justsml@users.noreply.github.com>
2017-04-10 14:01:44 -0600

    added some smarts for simpler usage

--- modules/devops-tools.sh
+++ modules/devops-tools.sh
@@ -1,24 +1,45 @@
 #!/bin/bash
 set -e
 
+## Installs CLI Client Binaries for common DevOps tools.
+## For Linux & OSX. 
+
 export  RANCHER_COMPOSE_VERSION=0.12.2
 export  DOCKER_COMPOSE_VERSION=1.11.2 
 export  DOCKER_VERSION=1.13.1
 
-### Install binaries ###
-sudo curl -fsSLO https://get.docker.com/builds/Linux/x86_64/docker-$DOCKER_VERSION.tgz
-sudo tar --strip-components=1 -xzf docker-$DOCKER_VERSION.tgz -C /usr/local/bin 
-sudo chmod +x /usr/local/bin/docker
+### Install/copy CLI binaries ###
 
-sudo curl -sL "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" > /usr/local/bin/docker-compose 
-sudo chmod +x /usr/local/bin/docker-compose
+function installDocker() {
+  sudo curl -fsSLO https://get.docker.com/builds/Linux/x86_64/docker-$DOCKER_VERSION.tgz
+  sudo tar --strip-components=1 -xzf docker-$DOCKER_VERSION.tgz -C /usr/local/bin 
+  sudo chmod +x /usr/local/bin/docker
+}
 
-sudo curl -sL "https://github.com/rancher/rancher-compose/releases/download/$RANCHER_COMPOSE_VERSION/rancher-compose-linux-amd64-$RANCHER_COMPOSE_VERSION.tar.gz" > /tmp/rancher-compose.tar.gz 
-sudo tar --strip-components=1 -xzf /tmp/rancher-compose.tar.gz -C /usr/local/bin 
-sudo chmod +x /usr/local/bin/rancher-compose
+function installDockerCompose() {
+  sudo curl -sL "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" > /usr/local/bin/docker-compose 
+  sudo chmod +x /usr/local/bin/docker-compose
+}
 
-printf "\n\nVersions:\n"
-printf "  Docker: $(docker --version)\n"
-printf "  Docker Compose: $(docker-compose --version)\n"
-printf "  Rancher Compose: $(rancher-compose --version)\n"
-printf "===============================\n\n\n"
+function installRancherCompose() {
+  sudo curl -sL "https://github.com/rancher/rancher-compose/releases/download/$RANCHER_COMPOSE_VERSION/rancher-compose-linux-amd64-$RANCHER_COMPOSE_VERSION.tar.gz" > /tmp/rancher-compose.tar.gz 
+  sudo tar --strip-components=1 -xzf /tmp/rancher-compose.tar.gz -C /usr/local/bin 
+  sudo chmod +x /usr/local/bin/rancher-compose
+}
+
+
+printf "\n\n\t*** Checking/Installing CLI Client Binaries for Docker & Rancher ***\t\n"
+printf "\nCurrent Version(s):\n"
+printf "\tDocker:\t\t\t$(docker --version 2>/dev/null)\n"
+printf "\tDocker Compose:\t\t$(docker-compose --version 2>/dev/null)\n"
+printf "\tRancher Compose:\t\t$(rancher-compose --version 2>/dev/null)\n"
+printf "===============================\n"
+
+
+printf "\nDocker & Rancher Versions:\n"
+[ "$(which docker)" == "" ]           && installDocker          || printf "\tDocker:\t\t\t$(docker --version 2>&1)\n"
+[ "$(which docker-compose)" == "" ]   && installDockerCompose   || printf "\tDocker Compose:\t\t$(docker-compose --version 2>&1)\n"
+[ "$(which rancher-compose)" == "" ]  && installRancherCompose  || printf "\tRancher Compose:\t\t$(rancher-compose --version 2>&1)\n"
+printf "===============================\n\n"
+
+
